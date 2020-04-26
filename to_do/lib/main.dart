f9f837d6b20ac1cc158b259eb3a56b1dd51376be
ikjot-2605 <ikjotsd@gmail.com>
2020-04-26 14:32:28 +0530

    Added working todo functionality with SharedPrefernces

--- to_do/lib/main.dart
+++ to_do/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+TextEditingController myController=new TextEditingController();
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
@@ -7,7 +11,6 @@ void main() {
       backgroundColor: Colors.black,
     ),
     home: MyApp(),
-
   ));
 }
 
@@ -17,10 +20,46 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String date=DateTime.now().toString().substring(0,10);
+  String imp='';
+  String date = DateTime.now().toString().substring(0, 10);
   // This widget is the root of your application.
+  Future<void> deleteString() async{
+    final prefs=await SharedPreferences.getInstance();
+    prefs.remove('stringValue');
+  }
+  Future<String> getStringValuesSF() async {
+    final prefs = await SharedPreferences.getInstance();
+    final stringValue = await prefs.getString('stringValue');
+    return stringValue??'';
+  }
+  Future<String> removeString(String toRemove) async {
+    final prefs = await SharedPreferences.getInstance();
+    final currString=await prefs.getString('stringValue');
+    String nowString=currString.replaceAll(("~"+toRemove),'');
+    prefs.setString('stringValue', nowString);
+    setState(() {
+      imp=nowString;
+    });
+  }
+  Future<void> _incrementStartup(String todo) async{
+    final prefs = await SharedPreferences.getInstance();
+    String last=await getStringValuesSF();
+    String current=last+"~"+todo;
+    await prefs.setString('stringValue', current);
+    setState(() {
+      imp=current;
+    });
+  }
+  void initState() {
+    getStringValuesSF().then((value){
+      imp=value;
+      print('Async done');
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
+    List<String> finalList=imp.split('~');
     return Scaffold(
       backgroundColor: Colors.grey[800],
       floatingActionButton: FloatingActionButton(
@@ -36,7 +75,6 @@ class _MyAppState extends State<MyApp> {
           style: TextStyle(
             fontSize: 25.0,
             fontWeight: FontWeight.bold,
-
           ),
         ),
         centerTitle: true,
@@ -45,63 +83,49 @@ class _MyAppState extends State<MyApp> {
         contentPadding: EdgeInsets.all(10.0),
         textColor: Colors.grey[300],
         selectedColor: Colors.grey[200],
-        child: ListView(
-          children: <Widget>[
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-            Card(
-              color: Colors.grey[800],
-              child: ListTile(
-                title: Center(child: Text('Item')),
-              ),
-            ),
-          ],
+        child: ListView.builder(
+          itemCount: finalList.length,
+          itemBuilder: (BuildContext context,int index){
+            if(finalList[index]==''){return Container(height:0.0,);}
+            return Column(
+              mainAxisAlignment:MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(19.0),
+                  child: Container(
+                    height: 50.0,
+                    child: Card(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            finalList[index],
+                            style: TextStyle(letterSpacing: 1.0),
+                          ),
+                          SizedBox(width: 60.0,),
+                          IconButton(
+                            icon: Icon(Icons.check),
+                            color: Colors.green,
+                            iconSize: 30.0,
+                            onPressed: (){
+                              String toRemove=finalList[index];
+                              removeString(toRemove);
+                              bool _visible = true;
+                              setState(() {
+                                _visible = !_visible;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
@@ -114,6 +138,7 @@ class _MyAppState extends State<MyApp> {
           Container(
             height: 200.0,
             child: TextField(
+              controller: myController,
               keyboardType: TextInputType.multiline,
               maxLines: null,
             ),
@@ -123,11 +148,15 @@ class _MyAppState extends State<MyApp> {
           ),
           FlatButton(
             child: Text('Add a To-Do'),
-            onPressed: (){
-              setState(() {
+            onPressed: () {
+              String todo=myController.text;
+              _incrementStartup(todo);
+              print (todo);
+              print (imp);
+              Navigator.pop(context);
 
-              });
             },
+
           ),
         ],
       ),
